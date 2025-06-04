ab744a5acd2208602234be6beeec4c36d02b2f76
Fanekena-dev <fanekenaraharijaona@gmail.com>
2025-06-04 06:14:52 +0200

    v0.0.1
    Content:
    - Added game's window settings
    - Displaying game's window
    - Added game's main panel settings
    - Added game's main panel
    - Added game loop in it's own separate Thread

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import javax.swing.JFrame;
+
 public class App {
   public static void main(String[] args) throws Exception {
+    JFrame window = new JFrame();
+
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setResizable(false);
+    window.setTitle("RetroPG");
+
+    GamePanel gamePanel = new GamePanel();
+    window.add(gamePanel);
     
+    window.pack();
+
+    // the window will be displayed at the center of the screen
+    window.setLocationRelativeTo(null);
+    window.setVisible(true);
   }
 }
